8b1ac9d43f31c211f3ecc5ab5499a1f6ae1de69a
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-03-18 09:01:30 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +9,52 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  #create
+  get '/articles/new' do
+    @article = Article.new
+    # redirect to '/articles'
+    erb :new
+  end
+
+  #read
+  get '/articles' do
+    @articles = Article.all
+    # binding.pry
+    erb :index
+  end
+
+  #create
+
+  post '/articles' do 
+    # binding.pry
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+    erb :new
+   
+  end
+  #read
+  get 'articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete 'articles/:id' do 
+    Article.delete(params[:id])
+    redirect to "/articles"
+  end
+
 end
